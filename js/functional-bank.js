66c2693943f00a99e2a83256000b586d24ae059e
DEWAN <dewanmahinchy@gmail.com>
2023-01-08 23:04:18 +0600

    updateTotal function code implemented

--- js/functional-bank.js
+++ js/functional-bank.js
@@ -8,20 +8,22 @@ function getInputValue(inputId){
     return inputAmount;
 }
 
+// updateTotal() used for update transactions details
+function updateTotal(totalId, amount){
+    const totalAmount = document.getElementById(totalId);
+    const amountText = totalAmount.innerText;
+    const previousAmount = parseFloat(amountText);
+    const newTotalAmount = previousAmount + amount;
+    totalAmount.innerText = newTotalAmount;
+}
+
 // handle deposit
 document.getElementById('deposit-btn').addEventListener('click', function(){
     // get the amount deposited
-    /* const depositInput = document.getElementById('deposit-input');
-    const newDepositText = depositInput.value;
-    const newDepositAmount = parseFloat(newDepositText); */
     const newDepositAmount = getInputValue('deposit-input');
     
     // set new deposit amount
-    const depositTotal = document.getElementById('deposit-total');
-    const depositText = depositTotal.innerText;
-    const previousDepositAmount = parseFloat(depositText);
-    const newDepositTotal = previousDepositAmount + newDepositAmount;
-    depositTotal.innerText = newDepositTotal;
+    updateTotal('deposit-total', newDepositAmount);
 
     // update account balance
     const balanceTotal = document.getElementById('balance-total');
@@ -34,17 +36,10 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
 // handle withdraw
 document.getElementById('withdraw-btn').addEventListener('click', function(){
     // get the amount Withdrew
-    /* const withdrawInput = document.getElementById('withdraw-input');
-    const newWithdrawText = withdrawInput.value;
-    const newWithdrawAmount = parseFloat(newWithdrawText); */
     const newWithdrawAmount = getInputValue('withdraw-input');
 
     // set new withdraw amount
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const withdrawText = withdrawTotal.innerText;
-    const previousWithdrawAmount = parseFloat(withdrawText);
-    const newWithdrawTotal = previousWithdrawAmount + newWithdrawAmount;
-    withdrawTotal.innerText = newWithdrawTotal;
+    updateTotal('withdraw-total', newWithdrawAmount);
 
     // update account balance
     const balanceTotal = document.getElementById('balance-total');
